701525e4f285152a612f9893d240a33a41aaf5d5
Kare Lehtonen <lehtonenkare@gmail.com>
2023-10-16 19:47:27 +0300

    Added simple documentation comments.

--- src/main.rs
+++ src/main.rs
@@ -1,18 +1,24 @@
 use clap::Parser;
 
+/// Struct for reverb option and input.
 #[derive(Parser)]
 struct Arguments {
 
-    /// Should we print given words one by one?
+    /// Whether to print the input on one line or multiple.
     #[arg(short = 's', long = "separate", action = clap::ArgAction::Count)]
     separate: u8,
 
-    /// String to print.
+    /// Input to be printed.
     input: Vec<String>
 }
 
 fn main() {
+    // Parse the given arguments.
     let arguments = Arguments::parse();
+
+    // Check if `separate` option is used.
+    // 0 = do not separate.
+    // 0 < = separate.
     if arguments.separate == 1 {
         print_input_separate(arguments.input);
     } else {
@@ -21,10 +27,12 @@ fn main() {
 }
 
 fn print_input_joined(input: Vec<String>) {
+    // Print input joined with spaces.
     println!("{}", input.join(" "));
 }
 
 fn print_input_separate(input: Vec<String>) {
+    // Print input separately one at a time.
     for string in input.iter() {
         println!("{}", string);
     };
