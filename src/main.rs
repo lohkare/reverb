8f9fcb1e4c6d5f5358aaa9401bf605f4f92e66b2
Kare Lehtonen <lehtonenkare@gmail.com>
2023-10-16 16:32:50 +0300

    Revert "Added basic functionality."
    This reverts commit 3b3a78fb4ab7f24389f2206a5311bf94e2baf0da.

--- src/main.rs
+++ src/main.rs
@@ -1,31 +1,3 @@
-use clap::Parser;
-
-#[derive(Parser)]
-struct Arguments {
-
-    /// Should we print given words one by one?
-    #[arg(short = 's', long = "separate", action = clap::ArgAction::Count)]
-    separate: u8,
-
-    /// String to print.
-    input: Vec<String>
-}
-
 fn main() {
-    let arguments = Arguments::parse();
-    if arguments.separate == 1 {
-        print_input_separate(arguments.input);
-    } else {
-        print_input_joined(arguments.input);
-    }
-}
-
-fn print_input_joined(input: Vec<String>) {
-    println!("{}", input.join(" "));
-}
-
-fn print_input_separate(input: Vec<String>) {
-    for string in input.iter() {
-        println!("{}", string);
-    };
+    println!("Hello, world!");
 }
