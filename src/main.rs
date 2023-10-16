72f2fb35f47f3fa6797bf5f397b7d163a761bcb0
Kare Lehtonen <lehtonenkare@gmail.com>
2023-10-16 16:35:49 +0300

    Added basic functionality. - Input can now be printed separately or as one.

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,31 @@
+use clap::Parser;
+
+#[derive(Parser)]
+struct Arguments {
+
+    /// Should we print given words one by one?
+    #[arg(short = 's', long = "separate", action = clap::ArgAction::Count)]
+    separate: u8,
+
+    /// String to print.
+    input: Vec<String>
+}
+
 fn main() {
-    println!("Hello, world!");
+    let arguments = Arguments::parse();
+    if arguments.separate == 1 {
+        print_input_separate(arguments.input);
+    } else {
+        print_input_joined(arguments.input);
+    }
+}
+
+fn print_input_joined(input: Vec<String>) {
+    println!("{}", input.join(" "));
+}
+
+fn print_input_separate(input: Vec<String>) {
+    for string in input.iter() {
+        println!("{}", string);
+    };
 }
