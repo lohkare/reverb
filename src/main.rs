f3431af087e93fac516b8e19a4dacbaf3b3405c7
Kare Lehtonen <lehtonenkare@gmail.com>
2023-10-17 10:47:19 +0300

    Added new copy -option. - Output is copied into clipboard.

--- src/main.rs
+++ src/main.rs
@@ -1,39 +1,48 @@
 use clap::Parser;
+use copypasta::{ClipboardContext, ClipboardProvider};
 
 /// Print input to stdout.
 #[derive(Parser)]
 struct Arguments {
 
+    /// Copy the stdout print to your clipboard. Cannot be used with separate.
+    /// Copy will take priority over separate.
+    #[arg(short = 'c', long = "copy", action = clap::ArgAction::SetTrue, verbatim_doc_comment)]
+    copy: bool,
+
     /// Whether to print the input on one line or multiple.
-    #[arg(short = 's', long = "separate", action = clap::ArgAction::Count)]
-    separate: u8,
+    /// Does not take effect if used with copy option.
+    #[arg(short = 's', long = "separate", action = clap::ArgAction::SetTrue, verbatim_doc_comment)]
+    separate: bool,
 
     /// Input to be printed.
     input: Vec<String>
 }
 
 fn main() {
-    // Parse the given arguments.
+    // Parse arguments.
     let arguments = Arguments::parse();
 
-    // Check if `separate` option is used.
-    // 0 = do not separate.
-    // 0 < = separate.
-    if arguments.separate == 1 {
-        print_input_separate(arguments.input);
-    } else {
-        print_input_joined(arguments.input);
+    if arguments.copy {
+        // Set up ClipboardContext.
+        let mut clipboard_context = ClipboardContext::new().unwrap();
+        // Join input to one string.
+        let output = arguments.input.join(" ");
+
+        // Set joined input to clipboard.
+        clipboard_context.set_contents(output.to_owned()).unwrap();
+        // Print result.
+        println!("{}", output)
     }
-}
 
-fn print_input_joined(input: Vec<String>) {
-    // Print input joined with spaces.
-    println!("{}", input.join(" "));
-}
+    if arguments.separate {
+        // Print input separately one at a time.
+        for string in arguments.input.iter() {
+            println!("{}", string);
+        }
 
-fn print_input_separate(input: Vec<String>) {
-    // Print input separately one at a time.
-    for string in input.iter() {
-        println!("{}", string);
-    };
+        return
+    }
+
+    println!("{}", arguments.input.join(" "));
 }
